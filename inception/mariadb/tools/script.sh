f2f466b6d423c3ea066c655b42952facb65e3fc3
hamzak99 <kasbaouihamza99@gmail.com>
2024-02-24 12:33:08 -0500

    ok

--- inception/mariadb/tools/script.sh
+++ inception/mariadb/tools/script.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# echo "\
-# FLUSH PRIVILEGES;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';
-# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-# CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
-# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%';
-# FLUSH PRIVILEGES;" > /db.sql
-
-# mariadbd --user=mysql --bootstrap < /db.sql
-
-# exec "$@"
 echo "\
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'admin';
--- inception/wordpress/tools/script.sh
+++ inception/wordpress/tools/script.sh
@@ -1,7 +1,11 @@
 #!bin/sh
 
+
+cd /var/www/html/wordpress
+ls 
+rm -rf *
+ls
 echo "creating folder /var/www/html/wordpress"
-cd /var/www/html/wordpress;
 if ! wp --allow-root core is-installed; then
     echo "wordpress is not installed"
 	wp core download	--allow-root; # download core files
@@ -12,32 +16,15 @@ echo "wordpress is installed"
 echo "first ifffff"
 wp user list --allow-root --path=/var/www/html/wordpress --field=user_login | grep -q admin
 if [ $? != 0 ]; then
+	echo "dkhalt "
 	wp user create --allow-root admin admin@gmail.com --role=administrator --user_pass=admin --path=/var/www/html/wordpress; # create first user (administrator)
 fi
 echo "secend ifffff"
 wp user list --allow-root --path=/var/www/html/wordpress --field=user_login | grep -q user
 if [ $? != 0 ]; then
+	echo "dkhalt "
 	wp user create --allow-root user user@gmail.com --role=editor --user_pass=password --path=/var/www/html/wordpress; # create second user (editor)
 fi
+echo "finshed"
 
 exec "$@"
-
-
-
-# cd /var/www/html/wordpress;
-# if ! wp --allow-root core is-installed; then
-# 	wp core download	--allow-root; # download core files
-# 	wp config create	--allow-root --dbname=${database1} --dbuser=${user} --dbpass=${password} --dbhost=mariadb:3306; # create the config file of wordpress that contain database info
-# 	wp core install		--allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL}; # create new wordpress website
-# fi
-
-# wp user list --allow-root --path=/var/www/html/wordpress --field=user_login | grep -q ${WP_ADMIN}
-# if [ $? != 0 ]; then
-# 	wp user create --allow-root ${WP_ADMIN} ${WP_ADMIN_EMAIL} --role=administrator --user_pass=${WP_ADMIN_PASSWD} --path=/var/www/html/wordpress; # create first user (administrator)
-# fi
-# wp user list --allow-root --path=/var/www/html/wordpress --field=user_login | grep -q ${WP_USER}
-# if [ $? != 0 ]; then
-# 	wp user create --allow-root ${WP_USER} ${WP_USER_EMAIL} --role=editor --user_pass=${WP_USER_PASSWD} --path=/var/www/html/wordpress; # create second user (editor)
-# fi
-
-# exec "$@"
